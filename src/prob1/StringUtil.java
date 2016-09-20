00fc6477ce40e8b02d3f13d95ba49a9e6b5dfb9d
motherfather <BITUSER@BITPC>
2016-09-20 16:23:34 +0900

    Finish!!!!!

--- src/prob1/StringUtil.java
+++ src/prob1/StringUtil.java
@@ -5,7 +5,8 @@ public class StringUtil {
     public static String concatenate( String[] str ){
         //문자열을 결합하여 리턴하는 메소드 구현
     	for(String i : str) {
-    		tmp += i;
+//    		tmp += i;
+    		tmp = tmp.concat(i);
     	}
     	return tmp;
     }
